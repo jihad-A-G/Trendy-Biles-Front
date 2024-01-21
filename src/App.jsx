58574be8c29c56d119a30c2356ba50618ffadbe6
Mohamad-Younes <142875332+Mohamadalyouness@users.noreply.github.com>
2024-01-21 13:39:11 +0200

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
 // import Navbar from "./Components/Navbar/Navbar.jsx"
-import Footer from "./Components/Footer/Footer.jsx"
+// import Footer from "./Components/Footer/Footer.jsx"
 // import RegisterLogin from "./Pages/RegisterPage/RegisterPage.jsx";
 function App() {
   return (
     <>
-      <div>
-     {/* <Navbar/> */}
-     <Footer/>
-      </div>
+      <div></div>
     </>
   );
 }
