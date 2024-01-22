b5afc32c6ca24a14b86b3be2d0ecc03fd9b56241
Mohamad-Younes <142875332+Mohamadalyouness@users.noreply.github.com>
2024-01-22 15:45:41 +0200

    NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Outlet,
+  Route
 } from "react-router-dom";
-import "./App.css";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 import ShippingPolicy from "./Pages/ShippingPolicyPage/ShippingPolicyPage.jsx";
@@ -13,19 +11,59 @@ import CustomerSupport from "./Pages/CustomerSupportPage/CustomerSupportPage.jsx
 import PriceMatchingPolicy from "./Pages/PriceMatchingPolicyPage/PriceMatchingPolicyPage.jsx";
 import RegisterPage from "./Pages/RegisterPage/RegisterPage.jsx";
 import NotFoundPage from "./Pages/NotFoundPage/NotFound.jsx";
+import "./App.css";
+
+const DefaultRoutes = () => {
+  const [error, setError] = useState(false);
+
+  const handleNavigationError = () => {
+    console.error("Error: Invalid path");
+    setError(true);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/Shipping-Policy"
+          element={<ShippingPolicy />}
+          onEnter={handleNavigationError}
+        />
+        <Route
+          path="/Customer-Support"
+          element={<CustomerSupport />}
+          onEnter={handleNavigationError}
+        />
+        <Route
+          path="/Price-Matching-Policy"
+          element={<PriceMatchingPolicy />}
+          onEnter={handleNavigationError}
+        />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+
 
 function App() {
   return (
     <Router>
-      <Navbar/>
       <Routes>
+      <Route
+          path="*"
+          element={
+            <div>
+              <DefaultRoutes />
+            </div>
+          }
+        />
+        <Route path="*" element= {<NotFoundPage/>} />
         <Route path="/Register" element={<RegisterPage />} />
-        <Route path="Shipping-Policy" element={<ShippingPolicy />} />
-        <Route path="Customer-Support" element={<CustomerSupport />} />
-        <Route path="Price-Matching-Policy" element={<PriceMatchingPolicy />} />
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
+   
       </Routes>
-      <Footer/>
     </Router>
   );
 }
--- src/Pages/RegisterPage/RegisterPage.jsx
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -1,68 +1,115 @@
-import React from 'react';
-import './RegisterPage.css';
+import React from "react";
+import "./RegisterPage.css";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import FullLogo from "../../assets/FullLogo2.jpg";
 const RegisterLogin = () => {
+  const navigate = useNavigate();
+
   const handleSignUpClick = () => {
-    document.getElementById('container').classList.add('right-panel-active');
+    document.getElementById("container").classList.add("right-panel-active");
   };
 
   const handleSignInClick = () => {
-    document.getElementById('container').classList.remove('right-panel-active');
+    document.getElementById("container").classList.remove("right-panel-active");
+  };
+
+  const handleGoHomeClick = () => {
+    // Use the navigate function to go back to the homepage
+    navigate("/");
   };
 
   return (
-    <div className='RegisterLoginPage'>
-      <div className="container" id="container">
-        <div className="form-container sign-up-container">
-          <form action="#">
-            <h1>Create Account</h1>
-            {/* <div className="social-container">
+    <>
+      <button className="go-home" onClick={handleGoHomeClick}>
+        Go Home
+      </button>
+      <div className="RegisterLoginPage">
+        <div className="container" id="container">
+          <div className="form-container sign-up-container">
+            <form action="#">
+              <h1>Create Account</h1>
+              {/* <div className="social-container">
               <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
               <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
               <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
             </div> */}
-            <span>or use your email for registration</span>
-            <input className='Registerinput' type="text" placeholder="UserName" />
-            <input className='Registerinput' type="email" placeholder="Email" />
-            <input className='Registerinput' type="password" placeholder="Password" />
-            <input className='Registerinput' type="text" placeholder="PhoneNumber" />
-            <input className='Registerinput' type="text" placeholder="Address" />
-            <button>Sign Up</button>
-          </form>
-        </div>
-        <div className="form-container sign-in-container">
-          <form action="#">
-            <h1>Sign in</h1>
-            {/* <div className="social-container">
+              <span>or use your email for registration</span>
+              <input
+                className="Registerinput"
+                type="text"
+                placeholder="UserName"
+              />
+              <input
+                className="Registerinput"
+                type="email"
+                placeholder="Email"
+              />
+              <input
+                className="Registerinput"
+                type="password"
+                placeholder="Password"
+              />
+              <input
+                className="Registerinput"
+                type="text"
+                placeholder="PhoneNumber"
+              />
+              <input
+                className="Registerinput"
+                type="text"
+                placeholder="Address"
+              />
+              <button>Sign Up</button>
+            </form>
+          </div>
+          <div className="form-container sign-in-container">
+            <form action="#">
+              <h1>Sign in</h1>
+              {/* <div className="social-container">
               <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
               <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
               <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
             </div> */}
-            <span> use your account</span>
-            <input className='Registerinput' type="email" placeholder="Email" />
-            <input className='Registerinput' type="password" placeholder="Password" />
-            <a href="#">Forgot your password?</a>
-            <button>Sign In</button>
-          </form>
-        </div>
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-left">
-                <img className='FullLogo' src={FullLogo}></img>
-              <h1>Welcome Back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
-              <button className="ghost" onClick={handleSignInClick}>Sign In</button>
-            </div>
-            <div className="overlay-panel overlay-right">
-            <img className='FullLogo' src={FullLogo}></img>
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start the journey with us</p>
-              <button className="ghost" onClick={handleSignUpClick}>Sign Up</button>
+              <span> use your account</span>
+              <input
+                className="Registerinput"
+                type="email"
+                placeholder="Email"
+              />
+              <input
+                className="Registerinput"
+                type="password"
+                placeholder="Password"
+              />
+              <a href="#">Forgot your password?</a>
+              <button>Sign In</button>
+            </form>
+          </div>
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-left">
+                <img className="FullLogo" src={FullLogo}></img>
+                <h1>Welcome Back!</h1>
+                <p>
+                  To keep connected with us please login with your personal info
+                </p>
+                <button className="ghost" onClick={handleSignInClick}>
+                  Sign In
+                </button>
+              </div>
+              <div className="overlay-panel overlay-right">
+                <img className="FullLogo" src={FullLogo}></img>
+                <h1>Hello, Friend!</h1>
+                <p>Enter your personal details and start the journey with us</p>
+                <button className="ghost" onClick={handleSignUpClick}>
+                  Sign Up
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-</div>
+    </>
   );
 };
 
