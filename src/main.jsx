9fe55228981e88046f76b440d9b1c12d69992fbc
jihad-A-G <jihadabdlghani73@gmail.com>
2024-01-23 15:28:19 +0200

    fixing conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import AdminLayout from './adminDashboard/dashboardLayout.jsx';
-// import App from './App.jsx';
+import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AdminLayout />
-    {/* <App/> */}
+    <App/>
     <ToastContainer />
   </React.StrictMode>,
 );
