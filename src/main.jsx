f004ee59ce38f51ab09d74f08c5f0b95676bc5f3
jihad-A-G <jihadabdlghani73@gmail.com>
2024-01-23 22:27:07 +0200

    Complete missing part from router

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-// import App from './App.jsx';
 import router from './config/router.jsx';
 import {RouterProvider } from 'react-router-dom';
-import axios from 'axios';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './Pages/HomePage/HomePage';
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
