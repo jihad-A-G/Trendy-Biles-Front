4167dc81eadb3206159579e7e237a114ed3c26f6
Mohamad-Younes <142875332+Mohamadalyouness@users.noreply.github.com>
2024-01-24 08:54:36 +0200

    n

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { useLoaderData } from 'react-router'
-export default function HomePage() {
-  const data = useLoaderData()
-  console.log(data);
-  return (
-    <div>{data.aboutusContent.content}</div>
-  )
-}
-//  import React from 'react'
+// import React from 'react'
+// import { useLoaderData } from 'react-router'
+// export default function HomePage() {
+//   const data = useLoaderData()
+//   console.log(data);
+//   return (
+//     <div>{data.aboutusContent.content}</div>
+//   )
+// }
+ import React from 'react'
  
-//  export default function HomePage() {
-//    return (
-//      <div>HomePage</div>
-//    )
-//  }
+ export default function HomePage() {
+   return (
+     <div></div>
+   )
+ }
  
--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-// import App from './App.jsx';
-// import router from './config/router.js';
-// import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-// import axios from 'axios';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './Pages/HomePage/HomePage';
-
-const router = createBrowserRouter([
-
-  {
-      path:"/",
-      element:<HomePage/>,
-      loader: async() =>{
-          const response = await axios.get('http://localhost:4000/api/aboutus/content')
-          console.log(response.data);
-
-          return response.data
-      }
-  },
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <RouterProvider router={router}/> */}
-    <ToastContainer />
-  </React.StrictMode>,
-);
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import { ToastContainer } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import App from './App.jsx';
+// // import App from './App.jsx';
+// // import router from './config/router.js';
+// // import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+// // import axios from 'axios';
 // import './index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import HomePage from './Pages/HomePage/HomePage';
 
+// const router = createBrowserRouter([
+
+//   {
+//       path:"/",
+//       element:<HomePage/>,
+//       loader: async() =>{
+//           const response = await axios.get('http://localhost:4000/api/aboutus/content')
+//           console.log(response.data);
+
+//           return response.data
+//       }
+//   },
+// ])
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
-//     <App />
+//     {/* <RouterProvider router={router}/> */}
 //     <ToastContainer />
 //   </React.StrictMode>,
 // );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <ToastContainer />
+  </React.StrictMode>,
+);
