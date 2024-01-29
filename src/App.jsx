568cb6eaa0c6a32dd314fd81a22ba0a8b77bddae
Mohamad-Younes <142875332+Mohamadalyouness@users.noreply.github.com>
2024-01-29 11:30:53 +0200

    Fixing css

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer/Footer.jsx";
 import NewNavbar from "./Components/NewNavbar/NewNav,.jsx";
-import ProductPage from "./Pages/ProductPage/ProductPage.jsx";
+// import ProductPage from "./Pages/ProductPage/ProductPage.jsx";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <NewNavbar/>
-      <ProductPage/>
+      {/* <ProductPage/> */}
 
       <Outlet />
       <Footer />
